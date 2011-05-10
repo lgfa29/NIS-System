8aebd4a4cd48488da6e77ea12dc7674f599adc5e
Luiz Aoqui <lgfa29@gmail.com>
2011-05-10 20:44:12 +0900

    create util methods

---
+++ src/nis/util/Util.java
@@ -0,0 +1,19 @@
+package nis.util;
+
+public class Util {
+
+	public static String parseLinks(String inputText) {
+		StringBuilder str = new StringBuilder();
+		String[] array = inputText.split(" ");
+		
+		for(String word : array){
+			if(word.startsWith("http://"))
+				str.append(" <a href=\"" + word + "\" target=\"_blank\"" +  ">" + word + "</a>");
+			else
+				str.append(" " + word);
+		}
+		
+		return str.toString();
+	}
+	
+}
